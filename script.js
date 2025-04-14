67d22262ebf52cdc523d6f1d07812bf1b64c76fc
lena mukhtar <lena.mukhtar@gmail.com>
2025-04-14 13:00:31 -0400

    adding the js code for get xml

--- script.js
+++ script.js
@@ -15,14 +15,46 @@ document.getElementById('fetchData').addEventListener('click', function() {
 });
 
 
+
+// XML Request Data (GET)
+document.getElementById('xhrData').addEventListener('click', function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) { 
+            if (xhr.status === 200) { 
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                displayData(data);
+            } else {
+                console.error('Error fetching data:', xhr.statusText);
+            }
+        }
+    };
+
+    xhr.send();
+});
+
+// display for both xml and fetch
 function displayData(data){
     const content = document.getElementById('output');
     content.innerHTML = ''; 
 
     const container = document.createElement('div');
 
+    const userLabel = document.createElement('label');
+    userLabel.textContent = "UserID: ";
+    const userValue = document.createElement('p');
+    userValue.textContent = data.userId;
+
+    const idLabel = document.createElement('label');
+    idLabel.textContent = "ID: ";
+    const idValue = document.createElement('p');
+    idValue.textContent = data.id;
+
     const titleLabel = document.createElement('label');
-    titleLabel.textContent="Title: ";
+    titleLabel.textContent="Title: " ;
     const titleValue= document.createElement('p');
     titleValue.textContent= data.title;
 
@@ -31,6 +63,10 @@ function displayData(data){
     const bodyValue= document.createElement('p');
     bodyValue.textContent = data.body;
 
+    container.appendChild(userLabel);
+    container.appendChild(userValue);
+    container.appendChild(idLabel);
+    container.appendChild(idValue);
     container.appendChild(titleLabel);
     container.appendChild(titleValue);
     container.appendChild(bodyLabel);
@@ -40,4 +76,3 @@ function displayData(data){
 
    
 }
-  
